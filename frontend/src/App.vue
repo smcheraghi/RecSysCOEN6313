29df9a87272eb801a58865b501877eae013a3a19
Dmitry Kryukov <remasik@gmail.com>
2018-10-03 13:33:48 -0400

    Add bootstrap and navigation example

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/ping">Ping</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -18,5 +23,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
