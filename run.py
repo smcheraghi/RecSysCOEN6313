f94866db04cc4d5630e1c1872da540cb62df525b
Dmitry Kryukov <remasik@gmail.com>
2018-10-03 13:04:26 -0400

    Init project

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+from random import *
+
+app = Flask(__name__,
+  static_folder = "./dist/static",
+  template_folder = "./dist")
+
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+@app.route('/api/random', methods=['GET'])
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+@app.route('/', defaults={'path': ''})
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
