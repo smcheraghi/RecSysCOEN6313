af28c1a241ed6bb9be88c8e9d5728b0f54f60066
Dmitry Kryukov <remasik@gmail.com>
2018-10-03 14:09:54 -0400

    Update structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/ping">Ping</router-link>
-    </div>
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
@@ -22,18 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,50 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-const routerOptions = [
-  { path: '/', component: 'Home' },
-  { path: '/ping', component: 'Ping' },
-  { path: '/about', component: 'About' },
-  { path: '*', component: '404' }
-]
+import Ping from '@/pages/Ping'
+import Home from '@/pages/Home'
+import About from '@/pages/About'
 
-const routes = routerOptions.map(route => {
-  return {
-    ...route,
-    component: () =>
-      import(`@/components/${route.component}.vue`)
-  }
-})
+// const routerOptions = [
+//   { path: '/', component: 'Home' },
+//   { path: '/ping', component: 'Ping' },
+//   { path: '/about', component: 'About' },
+//   { path: '*', component: '404' }
+// ]
 
+// const routes = routerOptions.map(route => {
+//   return {
+//     ...route,
+//     component: () =>
+//       import(`@/pages/${route.component}.vue`)
+//   }
+// })
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes,
+//   mode: 'history'
+// })
 Vue.use(Router)
 
 export default new Router({
-  routes,
-  mode: 'history'
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping
+    }
+  ]
 })
