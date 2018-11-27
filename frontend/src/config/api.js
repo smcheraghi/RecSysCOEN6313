0aee8f1175aa72696e9011cebe9078f923552f36
Dmitry Kryukov <remasik@gmail.com>
2018-11-26 22:00:04 -0500

    Update backend and query

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: 'http://localhost:5000/api/',
-    baseURL: 'http://localhost:3000/', // fake json server
+    baseURL: 'https://rec.wannaseethat.tk/api/',
+    // baseURL: 'http://localhost:3000/',
     json: true
     // headers: {
     //   'Access-Control-Allow-Origin': '*',
