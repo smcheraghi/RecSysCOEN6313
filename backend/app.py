d5baee56a3cd7a6125592ec3b5f10a77e8985d7c
baldandbrave <onthewayjing96@gmail.com>
2018-11-27 20:59:04 -0500

    add debug for json

--- backend/app.py
+++ backend/app.py
@@ -41,7 +41,7 @@ def get_bestseller():
 def post_behavior():
     actions = request.get_json()
     # need to know the type of var 'actions'
-
+    print(actions)
     rec_class = Inference(actions)
     rec = rec_class.inference(actions)
     return rec
