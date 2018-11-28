8a7d2eb5320402e0762b8185e3530bddf08fab39
baldandbrave <onthewayjing96@gmail.com>
2018-11-27 21:50:39 -0500

    fix var quote

--- backend/app.py
+++ backend/app.py
@@ -43,7 +43,7 @@ def post_behavior():
     # need to know the type of var 'actions'
     print(actions)
     rec_class = Inference(actions)
-    model = Model(rec_class.config,rec_class.cate_list)
+    model = Model(config,rec_class.cate_list)
     rec = rec_class.inference(actions)
     return rec
     # pass
